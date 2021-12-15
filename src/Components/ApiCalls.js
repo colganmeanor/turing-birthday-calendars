fe7f42d238b972681e732764506872b2df1b6a64
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-15 11:08:51 -0600

    Add birthdays to state upon successful fetch call in componentDidMount

--- src/Components/ApiCalls.js
+++ src/Components/ApiCalls.js
@@ -2,7 +2,7 @@
 const fetchBirthdays = () => {
     return fetch('http://localhost:3001/api/v1/birthdays')
             .then((response) => response.json())
-            .then((data) => console.log(data))
+            // .then((data) => console.log(data))
 }
 
 export default fetchBirthdays
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
 
   componentDidMount = () => {
     fetchBirthdays()
+    .then((data) => this.setState({birthdays: data}))
   }
 
   render =() => {
