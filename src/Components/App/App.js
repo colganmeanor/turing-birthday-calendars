0eedeb92a38403a83fa24863b2ae7e803222af30
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-15 10:46:18 -0600

    Test out birthday container component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import BirthdayContainer from '../BirthdayContainer';
 import { months } from '../../months_data';
 
 function App() {
@@ -10,10 +11,21 @@ function App() {
 
       </div>
       <div className='bday-container'>
-
+        <BirthdayContainer />
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+// psuedocode for iteration 1:
+
+// Get all existing birthdays on page load and display them on the DOM
+// each birthday needs to appear in the correct month. 
+
+// need to create birthday container component - DONE
+// need to fill out birthday container component as a functional component
+// that renders a box on the DOM for each month represented in the months_data file
+
---
+++ src/Components/BirthdayContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BirthdayContainer(){
+    return (
+    <p>Heeeeey this is the birthday container</p>
+    )
+}
+
+export default BirthdayContainer
